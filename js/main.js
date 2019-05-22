5f70c298920134dba14b99e5ccb86fae5670a225
Vadim Kiselev <vadim8kiselev@gmail.com>
2019-05-23 03:14:34 +0400

    Initial version of functionality

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    // Open menu
+    $('.toggler').click(function () {
+        $('.menu').toggleClass('menu-open');
+        $(this).toggleClass('toggler-active');
+    });
+
+    // If 'Change salary' was requested, open the dialog
+    $('#change-salary').click(function () {
+        to_entry();
+    });
+});
+
+function to_entry() {
+    window.location = 'index.html';
+}
+
