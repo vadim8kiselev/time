67a16a1ea07d43795135438cdc5d00dbf41dd02b
Vadim Kiselev <vadim8kiselev@gmail.com>
2020-01-19 19:31:42 +0400

    Separated controllers

--- src/main/java/com/kiselev/time/view/web/controller/IndexController.java
+++ src/main/java/com/kiselev/time/view/web/controller/IndexController.java
@@ -5,20 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class WebViewController {
+public class IndexController {
 
     @RequestMapping(value = {"/"}, method = RequestMethod.GET)
     public String index() {
         return "index";
-    }
-
-    @RequestMapping(value = {"/menu"}, method = RequestMethod.GET)
-    public String menu() {
-        return "menu";
-    }
-
-    @RequestMapping(value = {"/calculator"}, method = RequestMethod.GET)
-    public String calculator() {
-        return "calculator";
     }
 }
