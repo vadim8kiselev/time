91f37eb62645dac3458772a64436141aa4b818ee
Vadim Kiselev <vadim8kiselev@gmail.com>
2021-02-12 23:22:38 +0300

    Turn off security

--- README.md
+++ README.md
@@ -3,8 +3,4 @@ Exclusive calculator of time in the income measurement
 
 ### Heroku commands
 #### Redeployment
-heroku scale web=0; 
-git commit --allow-empty -m "Trigger Heroku deploy"; 
-git push heroku master; 
-heroku scale web=1; 
-heroku logs --tail;
+heroku scale web=0; git commit --allow-empty -m "Trigger Heroku deploy"; git push heroku master; heroku scale web=1; heroku logs --tail;
--- src/main/java/com/kiselev/time/security/SecurityConfiguration.java
+++ src/main/java/com/kiselev/time/security/SecurityConfiguration.java
@@ -22,8 +22,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private ProfileRepository profileRepository;
